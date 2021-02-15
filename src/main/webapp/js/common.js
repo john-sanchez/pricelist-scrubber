e71859960285af7ecf3da89c6d365e283bb7b513
john-sanchez <mail@johnsanchez.io>
2021-02-15 22:03:05 +0800

    refactor and added more functions

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -40,6 +40,36 @@ function deleteFromWatchlist(key) {
 	saveWatchlist(watchlistStore);
 }
 
+function renderDashboard() {
+		var watchlist = getWatchlist();
+		for (var key in watchlist) {
+			var watchdata = watchlist[key];
+			var card = $('<div class="card" style="width: 30rem; height: 500px;"></div>').appendTo("#widget-row");
+			var cardBody = $('<div class="card-body"></div>').appendTo(card).attr('id', 'card-body-'+key);
+			$('<h5 class="card-title"></h5>').text(watchdata.brand + ' ' + watchdata.properties + ' ' + watchdata.description).appendTo(cardBody);
+			
+			getPriceHist(key, function(data, labels, cdata, xdata) {
+				var cardBodyId = "#card-body-"+xdata.sku;
+				var canvas = $("<canvas style='width: 100%;height: 300px;'></canvas>").appendTo(cardBodyId);
+				var removeButton = $('<button class="btn btn-danger" aria-label="Left Align"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Remove</button>');
+				removeButton.appendTo(cardBodyId);
+				removeButton.click(() => {
+					deleteFromWatchlist(key);
+					$("#widget-row").empty();
+					renderDashboard();
+				});
+				var ctx = canvas[0].getContext("2d");
+				chartData(ctx, labels, cdata);
+			});
+		}
+	if (Object.keys(watchlist).length == 0) {
+		var html = `<div class="alert alert-primary" role="alert">
+			Your watchlist is empty, click <a href="/pricelist.html">here</a> to monitor prices.
+		</div>`
+		$("#widget-row").html(html);
+	}
+}
+
 function chartData(ctx, labels, cdata) {
 	new Chart(ctx, {
 					type : 'line',
@@ -63,3 +93,28 @@ function chartData(ctx, labels, cdata) {
 					}
 				});
 }
+
+function getNav(idx) {
+	return `
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/">Pricelist Scrubber</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+  	<ul class="navbar-nav">
+      <li class="nav-item ${idx == 0 ? 'active' : ''}">
+        <a class="nav-link" href="/">Dashboard</a>
+      </li>
+      <li class="nav-item ${idx == 1 ? 'active' : ''}">
+        <a class="nav-link" href="/pricelist.html">Price List</a>
+      </li>
+      <li class="nav-item ${idx == 2 ? 'active' : ''}">
+        <a class="nav-link" href="/canvas.html">PC Canvas Helper</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+	`;
+}
+
